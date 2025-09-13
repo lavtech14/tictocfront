0a6607b3756a02992c1a4462d98fed5f97ffec48
lavtech <lavtech14@gmail.com>
2025-09-13 19:32:13 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 import "./App.css";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://tictacback-98z7.onrender.com");
 
 // Winning combinations
 const WINNING_COMBINATIONS = [
